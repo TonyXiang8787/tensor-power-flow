29d3b6ca08f70eadaf7d23381b53bee8679e3630
Tony Xiang <tonyxiang@live.nl>
2024-06-26 11:38:15 +0200

    reorder graph
    Signed-off-by: Tony Xiang <tonyxiang@live.nl>

--- src/tensor_power_flow/tensor_power_flow.py
+++ src/tensor_power_flow/tensor_power_flow.py
@@ -2,13 +2,20 @@ from power_grid_model import PowerGridModel
 from power_grid_model.data_types import SingleDataset, BatchDataset
 import numpy as np
 from .data_checker import check_input
+import scipy.sparse as sp
+import scipy.sparse.csgraph as csg
+from typing import Optional
 
 
 class TensorPowerFlow:
     _input_data: SingleDataset
     _model: PowerGridModel
     _n_node: int
-    _node_reorder: np.ndarray
+    _n_line: int
+    _n_load: int
+    _node_reordered: bool
+    _node_reordered_to_org: Optional[np.ndarray]
+    _node_org_to_reordered: Optional[np.ndarray]
     _line_node_from: np.ndarray
     _line_node_to: np.ndarray
     _load_node: np.ndarray
@@ -19,12 +26,33 @@ class TensorPowerFlow:
         self._model = PowerGridModel(input_data, system_frequency)
         check_input(input_data)
         self._n_node = len(input_data["node"])
-        self._node_reorder = np.zeros(self._n_node, dtype=np.int64)
+        self._n_line = len(input_data["line"])
+        self._n_load = len(input_data["sym_load"])
+        self._node_reordered = False
+        self._node_reordered_to_org = None
+        self._node_org_to_reordered = None
         self._line_node_from = self._model.get_indexer("node", input_data["line"]["from_node"])
         self._line_node_to = self._model.get_indexer("node", input_data["line"]["to_node"])
         self._load_node = self._model.get_indexer("node", input_data["sym_load"]["node"])
         self._source_node = self._model.get_indexer("node", input_data["source"]["node"])[0]
-        self._graph_reorder()
 
     def _graph_reorder(self):
-        pass
+        edge_i = np.concatenate((self._line_node_from, self._line_node_to), axis=0)
+        edge_j = np.concatenate((self._line_node_to, self._line_node_from), axis=0)
+        connection_array = sp.csr_array(
+            (np.ones(self._n_line * 2), (edge_i, edge_j)), shape=(self._n_node, self._n_node)
+        )
+        reordered_node = csg.depth_first_order(
+            connection_array, self._source_node, directed=False, return_predecessors=False
+        )
+        if len(reordered_node) != self._n_node:
+            raise ValueError("The graph is not connected!")
+        reordered_node = reordered_node[::-1]
+        self._node_reordered_to_org = reordered_node
+        self._node_org_to_reordered = np.full(shape=(self._n_node,), dtype=np.int64, fill_value=-1)
+        self._node_org_to_reordered[reordered_node] = np.arange(self._n_node, dtype=np.int64)
+        self._line_node_from = self._node_org_to_reordered[self._line_node_from]
+        self._line_node_to = self._node_org_to_reordered[self._line_node_to]
+        self._load_node = self._node_org_to_reordered[self._load_node]
+        self._source_node = self._node_org_to_reordered[self._source_node]
+        self._node_reordered = True
