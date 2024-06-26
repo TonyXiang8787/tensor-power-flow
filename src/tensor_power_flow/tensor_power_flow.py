681f5d92519a8cad9cf2d3cd8da2b2c5e6396871
Tony Xiang <tonyxiang@live.nl>
2024-06-26 12:52:35 +0200

    finish y bus'
    Signed-off-by: Tony Xiang <tonyxiang@live.nl>

--- src/tensor_power_flow/tensor_power_flow.py
+++ src/tensor_power_flow/tensor_power_flow.py
@@ -10,28 +10,29 @@ from . import BASE_POWER
 
 class TensorPowerFlow:
     _input_data: SingleDataset
+    _system_frequency: float
     _model: PowerGridModel
     _u_rated: float
     _n_node: int
     _n_line: int
     _n_load: int
-    _node_reordered: bool
     _node_reordered_to_org: Optional[np.ndarray]
     _node_org_to_reordered: Optional[np.ndarray]
     _line_node_from: np.ndarray
     _line_node_to: np.ndarray
     _load_node: np.ndarray
     _source_node: int
+    _y_bus: Optional[sp.csc_array]
 
     def __init__(self, input_data: SingleDataset, system_frequency: float):
         self._input_data = input_data
+        self._system_frequency = system_frequency
         self._model = PowerGridModel(input_data, system_frequency)
         check_input(input_data)
         self._u_rated = input_data["node"]["u_rated"][0]
         self._n_node = len(input_data["node"])
         self._n_line = len(input_data["line"])
         self._n_load = len(input_data["sym_load"])
-        self._node_reordered = False
         self._node_reordered_to_org = None
         self._node_org_to_reordered = None
         self._line_node_from = self._model.get_indexer("node", input_data["line"]["from_node"])
@@ -59,4 +60,44 @@ class TensorPowerFlow:
         self._load_node = self._node_org_to_reordered[self._load_node]
         self._source_node = self._node_org_to_reordered[self._source_node]
         assert self._source_node == self._n_node - 1
-        self._node_reordered = True
+
+    def _build_y_bus(self):
+        # branch admitance matrix
+        y_series = 1.0 / (self._input_data["line"]["r1"] + 1j * self._input_data["line"]["x1"])
+        y_shunt = (
+            (0.5 * 2 * np.pi * self._system_frequency)
+            * self._input_data["line"]["c1"]
+            * (1j + self._input_data["line"]["tan1"])
+        )
+        all_y = np.concatenate((y_series, y_shunt, y_shunt), axis=0)
+        y_branch = sp.dia_array((all_y, 0), shape=(self._n_line * 3, self._n_line * 3))
+
+        # incidence matrix
+        # series_from, series_to, shunt_from, shunt_to
+        ones = np.ones(shape=(self._n_line,), dtype=np.int64)
+        incidence_entry = np.concatenate((ones, -ones, ones, ones), axis=0)
+        incidance_i = np.concatenate(
+            (self._line_node_from, self._line_node_to, self._line_node_from, self._line_node_to), axis=0
+        )
+        incidance_j = np.concatenate(
+            (
+                np.arange(self._n_line),
+                np.arange(self._n_line),
+                np.arange(self._n_line, self._n_line * 2),
+                np.arange(self._n_line * 2, self._n_line * 3),
+            ),
+            axis=0,
+        )
+        incidence_matrix = sp.csc_array(
+            (incidence_entry, (incidance_i, incidance_j)), shape=(self._n_node, self._n_line * 3)
+        )
+        self._y_bus = (incidence_matrix @ y_branch @ incidence_matrix.T).tocsc(copy=True)
+
+    def calculate_power_flow(
+        self, *, update_data: BatchDataset, max_iteration: int = 20, error_tolerance: float = 1e-8
+    ):
+        if self._node_reordered_to_org is None:
+            self._graph_reorder()
+        if self._y_bus is None:
+            self._build_y_bus()
+        # diag_pivot_thresh = 0.0
