d4e18c48a214547e6c891f2bdd0598fdb8d69ce7
Tony Xiang <tonyxiang@live.nl>
2024-06-27 11:24:07 +0200

    match result
    Signed-off-by: Tony Xiang <tonyxiang@live.nl>

--- src/tensor_power_flow/numba_functions.py
+++ src/tensor_power_flow/numba_functions.py
@@ -21,12 +21,12 @@ def set_rhs(rhs, load_pu, load_type, load_node, u, u_abs, i_ref):
         node_i = load_node[i]
         type_i = load_type[i]
         if type_i == CONST_POWER:
-            rhs[:, node_i] -= np.conj(load_pu[:, node_i] / u[:, node_i])
+            rhs[:, node_i] -= np.conj(load_pu[:, i] / u[:, node_i])
         elif type_i == CONST_CURRENT:
-            rhs[:, node_i] -= np.conj(load_pu[:, node_i] * u_abs[:, node_i] / u[:, node_i])
+            rhs[:, node_i] -= np.conj(load_pu[:, i] * u_abs[:, node_i] / u[:, node_i])
         elif type_i == CONST_IMPEDANCE:
             # formula: conj(s * u_abs^2 / u) = conj(s * u * conj(u) / u) = conj(s * conj(u)) = conj(s) * u
-            rhs[:, node_i] -= np.conj(load_pu[:, node_i]) * u[:, node_i]
+            rhs[:, node_i] -= np.conj(load_pu[:, i]) * u[:, node_i]
     rhs[:, -1] += i_ref
 
 
