d9401b3dcac6872adb5509bbb91d11f7f4912d9a
Tony Xiang <tonyxiang@live.nl>
2024-07-04 13:03:38 +0200

    try c order, slow
    Signed-off-by: Tony Xiang <tonyxiang@live.nl>

--- scripts/numba_cuda.py
+++ scripts/numba_cuda.py
@@ -17,6 +17,8 @@ def add_conjugate_kernel(ad, bd, cd, n_iter: int):
 
 def add_conjugate_cuda(b, c, n_iter: int):
     ad = cuda.device_array_like(b)
+    assert ad.is_c_contiguous() == b.flags["C_CONTIGUOUS"]
+    assert ad.is_f_contiguous() == b.flags["F_CONTIGUOUS"]
     bd = cuda.to_device(b)
     cd = cuda.to_device(c)
 
@@ -34,17 +36,18 @@ def add_conjugate_numpy(b, c, n_iter: int):
     return a
 
 
-def rnd_complex(shape, seed=0):
+def rnd_complex(shape, seed=0, c_contiguous=True):
     rng = np.random.default_rng(seed=seed)
-    return np.asfortranarray(
-        rng.uniform(high=1.0, low=0.0, size=shape) + 1j * rng.uniform(high=1.0, low=0.0, size=shape)
-    )
+    a = rng.uniform(high=1.0, low=0.0, size=shape) + 1j * rng.uniform(high=1.0, low=0.0, size=shape)
+    if not c_contiguous:
+        return np.asfortranarray(a)
+    return a
 
 
-def run_test(size, step, n_iter=100, print_output=False):
+def run_test(size, step, n_iter=100, print_output=False, c_contiguous=True):
     shape = (step, size)
-    b = rnd_complex(shape, 0)
-    c = rnd_complex(shape, 1)
+    b = rnd_complex(shape, seed=0, c_contiguous=c_contiguous)
+    c = rnd_complex(shape, seed=1, c_contiguous=c_contiguous)
 
     start_cuda = time.time()
     a_cuda = add_conjugate_cuda(b, c, n_iter=n_iter)
@@ -55,7 +58,7 @@ def run_test(size, step, n_iter=100, print_output=False):
     end_numpy = time.time()
 
     if print_output:
-        print(f"step: {step}, size: {size}")
+        print(f"step: {step}, size: {size}, c_contiguous: {c_contiguous}")
         print(f"Time taken for CUDA: {end_cuda - start_cuda} seconds")
         print(f"Time taken for NumPy: {end_numpy - start_numpy} seconds")
         diff = np.max(np.abs(a_cuda - a_numpy))
@@ -63,6 +66,12 @@ def run_test(size, step, n_iter=100, print_output=False):
 
 
 if __name__ == "__main__":
-    run_test(size=10, step=10_000, print_output=True)
-    run_test(size=100, step=100_000, print_output=True)
-    run_test(size=10, step=1_000_000, print_output=True)
+    # C order
+    # run_test(size=10, step=10_000, print_output=False, c_contiguous=True)
+    # run_test(size=100, step=100_000, print_output=True, c_contiguous=True)
+    # run_test(size=10, step=1_000_000, print_output=True, c_contiguous=True)
+    
+    # F order
+    run_test(size=10, step=10_000, print_output=False, c_contiguous=False)
+    run_test(size=100, step=100_000, print_output=True, c_contiguous=False)
+    run_test(size=10, step=1_000_000, print_output=True, c_contiguous=False)
